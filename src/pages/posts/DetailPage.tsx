9de23d93eb91ef94d787f8fa9baebc1026ac86a1
PixeIDark <kiki7951@naver.com>
2025-12-07 01:45:34 +0900

    feat: 상세 페이지 초기 세팅

--- src/pages/posts/DetailPage.tsx
+++ src/pages/posts/DetailPage.tsx
@@ -1,5 +1,24 @@
+import { useLocation } from "react-router-dom";
+import type { Post } from "../../types/post.ts";
+
+// TODO: 상세
+// 로그아웃 누르면 바로 나가져야함 로그인 상태를 전역으로 가져야할듯
 function DetailPage() {
-  return <div>detail</div>;
+  const location = useLocation();
+  const board: Post = location.state;
+
+  return (
+    <div>
+      <p>detail</p>
+      <div>
+        <h1>{board.title}</h1>
+        <p>{board.createdAt}</p>
+        <p>{board.body}</p>
+        <p>{board.tags}</p>
+        <p>{board.category}</p>
+      </div>
+    </div>
+  );
 }
 
 export default DetailPage;
