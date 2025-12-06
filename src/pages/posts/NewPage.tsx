3dbc63e90f942dbb095e84cfa7e486138240afe2
PixeIDark <kiki7951@naver.com>
2025-12-07 00:22:16 +0900

    feat: 게시글 작성 금칙어 추가

--- src/pages/posts/NewPage.tsx
+++ src/pages/posts/NewPage.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { postsApi } from "../../api/posts";
 import type { Category } from "../../types/post.ts";
+import { useNavigate } from "react-router-dom";
+import { PATHS } from "../../router/path.ts";
 
 const CATEGORY_OPTIONS: Record<Category, string> = {
   NOTICE: "알림",
@@ -8,11 +10,23 @@ const CATEGORY_OPTIONS: Record<Category, string> = {
   FREE: "자유",
 } as const;
 
+const BEN_WORDS = ["캄보디아", "프놈펜", "불법체류", "텔레그램"];
+
+// TODO: NewPage 해야할 것
+// 1. 디자인 상향
+// 2. 상수 분리
+// 3. 입력값 검증 함수로 분리
+// 4. 에러 처리
+// 5. 컴포넌트 분리
+// 6. 훅 분리
+// 7. ListPage 낙관적 업데이트 적용하기(오래걸릴듯)
+// => await 하고 이동 할꺼면 낙관적 업데이트 없어도되는데 await 없이 바로 이동이면 해줘야함
 function NewPage() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [tags, setTags] = useState("");
   const [category, setCategory] = useState<Category | "">("");
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -25,10 +39,17 @@ function NewPage() {
     };
 
     await postsApi.create(postData);
+    navigate(PATHS.POSTS.LIST);
   };
 
+  const hasBannedWord = BEN_WORDS.some((word) => title.includes(word) || body.includes(word) || tags.includes(word));
+
   const isActive =
-    title.trim().length > 0 && body.trim().length > 0 && tags.trim().length > 0 && category in CATEGORY_OPTIONS;
+    title.trim().length > 0 &&
+    body.trim().length > 0 &&
+    tags.trim().length > 0 &&
+    category in CATEGORY_OPTIONS &&
+    !hasBannedWord;
 
   return (
     <div className="w-full">
@@ -58,6 +79,7 @@ function NewPage() {
             </option>
           ))}
         </select>
+        {hasBannedWord && <p className="text-sm text-red-500">금칙어가 포함되어 있습니다</p>}
         <button disabled={!isActive} type="submit" className={`bg-blue-600 disabled:bg-gray-400`}>
           게시글 생성
         </button>
