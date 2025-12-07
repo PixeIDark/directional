35e9987b17694bf280309bd8cd28affc7152b21b
PixeIDark <kiki7951@naver.com>
2025-12-07 20:37:49 +0900

    feat: 수정 페이지 구현

--- src/pages/posts/EditPage.tsx
+++ src/pages/posts/EditPage.tsx
@@ -1,5 +1,78 @@
+import PostForm from "./components/PostForm.tsx";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import type { PostFormDataOutput } from "../../schemas/postSchema.ts";
+import { postsApi } from "../../api/posts.ts";
+import { PATHS } from "../../router/path.ts";
+import type { Post } from "../../types/post.ts";
+
 function EditPage() {
-  return <div>edit</div>;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { id } = useParams<{ id: string }>();
+  const [board, setBoard] = useState<Post | null>(location.state);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      if (!board && id) {
+        setIsLoading(true);
+        try {
+          const post = await postsApi.getDetail(id);
+          setBoard(post);
+        } catch (err) {
+          console.error("게시글 로드 실패:", err);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    };
+
+    fetchPost();
+  }, [board, id]);
+
+  if (!id) return null;
+
+  const handleFormSubmit = async (data: PostFormDataOutput) => {
+    setIsLoading(true);
+    try {
+      await postsApi.update(id, data);
+      navigate(PATHS.POSTS.DETAIL(id));
+    } catch (error) {
+      console.error("게시글 생성 실패:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleCancel = () => {
+    if (confirm("작성 중인 내용이 저장되지 않습니다. 취소하시겠습니까?")) navigate(PATHS.POSTS.DETAIL(id));
+  };
+
+  const defaultValue = {
+    title: board?.title ?? "",
+    body: board?.body ?? "",
+    tags: board?.tags.join(", ") ?? "",
+    category: board?.category,
+  };
+
+  return (
+    <div className="h-full w-full">
+      <div className="mb-6 flex items-center justify-between">
+        <h1 className="text-2xl font-bold">게시글 수정</h1>
+        <Link to={PATHS.POSTS.LIST} className="rounded bg-gray-500 px-4 py-2 text-white hover:bg-gray-600">
+          목록
+        </Link>
+      </div>
+      <PostForm
+        defaultValues={defaultValue}
+        onSubmit={handleFormSubmit}
+        onCancel={handleCancel}
+        submitButtonText="등록"
+        isLoading={isLoading}
+      />
+    </div>
+  );
 }
 
 export default EditPage;
