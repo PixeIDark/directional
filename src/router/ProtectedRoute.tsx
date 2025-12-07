ddc3adf2c526543c34c303bc92b1860d1aa9bbb8
PixeIDark <kiki7951@naver.com>
2025-12-08 03:10:48 +0900

    chore: home 아이콘 다운

--- src/router/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../context/AuthContext.tsx";
-
-export function ProtectedRoute() {
-  const { isLoggedIn } = useAuth();
-
-  if (!isLoggedIn) return <Navigate to="/" replace />;
-  return <Outlet />;
-}
