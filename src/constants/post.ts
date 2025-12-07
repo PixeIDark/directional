7c206c831cdcd308028e2f87429fefdd96f6c2af
PixeIDark <kiki7951@naver.com>
2025-12-07 18:52:04 +0900

    refactor: CATEGORIES 배열 타입으로 수정

--- src/constants/post.ts
+++ src/constants/post.ts
@@ -1,7 +1,3 @@
 import type { Category } from "../types/post.ts";
 
-export const CATEGORIES: Record<Category, string> = {
-  NOTICE: "알림",
-  QNA: "질문",
-  FREE: "자유",
-} as const;
+export const CATEGORIES: Category[] = ["NOTICE", "QNA", "FREE"];
